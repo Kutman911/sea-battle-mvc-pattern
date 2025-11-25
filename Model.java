d6072414b56d2acc732cbbe583cae9880673dbe7
Aikanysh28 <aikanysh2806@gmail.com>
2025-11-25 14:09:38 +0600

    feat: add game board 2D array to Model. ref #4

--- Model.java
+++ Model.java
@@ -1,7 +1,24 @@
 public class Model {
     private Viewer viewer;
+    private int[][] cellAreaComputer;
 
     public Model(Viewer viewer) {
         this.viewer = viewer;
+        cellAreaComputer = new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+    }
+
+    public int[][] getCellAreaComputer() {
+        return cellAreaComputer;
     }
 }
