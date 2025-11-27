f3480b94f5637e91101d98078f013d59d3c6b07e
Aikanysh28 <aikanysh2806@gmail.com>
2025-11-28 01:15:24 +0600

    fix: edited visibility of splash window

--- src/SplashWindow.java
+++ src/SplashWindow.java
@@ -8,11 +8,11 @@ public class SplashWindow {
 
     public SplashWindow() {
         window = new JWindow();
-        ImageIcon icon = new ImageIcon("images/splash.jpg");
+        ImageIcon icon = new ImageIcon("src/images/splash.jpg");
         JLabel label = new JLabel();
         label.setIcon(icon);
         window.add(label);
-        window.setSize(1184, 660);
+        window.setSize(1408, 768);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
@@ -21,3 +21,4 @@ public class SplashWindow {
         window.dispose();
     }
 }
+
