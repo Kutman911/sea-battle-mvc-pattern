a6ac8a7f332e87d7145f257928e8ac1733a8e030
bayell05 <beztelefons007@gmail.com>
2025-11-27 22:06:15 +0600

    #7: Implement Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Viewer viewer = new Viewer();
+
+        SplashWindow splashWindow = new SplashWindow();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException ie) {
+            System.err.println(ie);
+        }
+        splashWindow.closeWindow();
+        viewer.setVisibleFrame();
+
     }
 }
