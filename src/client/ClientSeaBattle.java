7dc48f4a61aae8db48f6641999a183f6a9ff58a5
Махир Джамалов <mahirdjamalov@gmail.com>
2025-12-15 13:51:17 +0600

    feat: added the initial work with the server

---
+++ src/client/ClientSeaBattle.java
@@ -0,0 +1,33 @@
+package client;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientSeaBattle implements Runnable {
+
+    private Socket clientSocket;
+    private Thread thread;
+
+    public ClientSeaBattle(Socket socket) {
+        this.clientSocket = socket;
+        thread = new Thread(this);
+    }
+
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+
+            String message = in.readLine();
+            System.out.println(message);
+
+        } catch (IOException ioe) {
+            System.out.println("Error into InputStream");
+        }
+    }
+
+    public void go() {
+        thread.start();
+    }
+
+}
