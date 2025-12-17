beed09d72740740c406fffa5f502f13eb8b85861
temirbaevabermet <temirbaevabermet4@gmail.com>
2025-12-17 23:51:08 +0600

    feat: Added a method for changing the volume in the AudioPlayer class ref:#51

--- src/common/AudioPlayer.java
+++ src/common/AudioPlayer.java
@@ -5,14 +5,16 @@ import java.io.File;
 import java.io.IOException;
 
 public class AudioPlayer {
-
     private Clip clip;
+    private float volume = 1.0f;
 
     public void playBackgroundMusic(String filename) {
         try {
             AudioInputStream audio = AudioSystem.getAudioInputStream(new File(filename));
             clip = AudioSystem.getClip();
             clip.open(audio);
+
+            setVolume(volume);
 
             clip.loop(Clip.LOOP_CONTINUOUSLY);
             clip.start();
@@ -26,6 +28,9 @@ public class AudioPlayer {
             AudioInputStream audio = AudioSystem.getAudioInputStream(new File(filename));
             Clip soundClip = AudioSystem.getClip();
             soundClip.open(audio);
+
+            setClipVolume(soundClip, volume);
+
             soundClip.start();
         } catch (Exception e) {
             e.printStackTrace();
@@ -37,10 +42,38 @@ public class AudioPlayer {
             AudioInputStream audio = AudioSystem.getAudioInputStream(new File(filename));
             Clip levelClip = AudioSystem.getClip();
             levelClip.open(audio);
+
+            setClipVolume(levelClip, volume);
+
             levelClip.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void setVolume(float volume) {
+        this.volume = Math.max(0.0f, Math.min(1.0f, volume)); // Ограничение 0.0-1.0
+        if (clip != null && clip.isOpen()) {
+            setClipVolume(clip, this.volume);
+        }
+    }
+
+    private void setClipVolume(Clip clip, float volume) {
+        if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+
+            float min = gainControl.getMinimum();
+            float max = gainControl.getMaximum();
+            
+            float gain = (float) (20.0 * Math.log10(volume));
+            gain = Math.max(min, Math.min(max, gain));
+
+            gainControl.setValue(gain);
+        }
+    }
+
+    public float getVolume() {
+        return volume;
     }
 
     public void stop() {
@@ -48,6 +81,4 @@ public class AudioPlayer {
             clip.stop();
         }
     }
-
-
 }
